e33322f7d27bb99511e79801aed3e5ac1ccaa825
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-02-22 10:06:48 -0300

    enhancing communication

--- project/cdc-admin/src/App.js
+++ project/cdc-admin/src/App.js
@@ -1,63 +1,9 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './components/InputCustomizado';
+import AutorBox from './Autor';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      lista: [],
-      nome: '',
-      email: '',
-      senha: ''
-    };
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: 'http://localhost:8080/api/autores',
-      dataType: 'json',
-      success: function(response) {
-        this.setState({lista:response});
-      }.bind(this)
-    });
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault();
-    $.ajax({
-      url: 'http://localhost:8080/api/autores',
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
-      success: function(resposta) {
-        this.setState({lista: resposta});
-      }.bind(this),
-      error: function(resposta) {
-        console.log('erro');
-      }
-    })
-  }
-
-  setNome(evento) {
-    this.setState({nome: evento.target.value});
-  }
-
-  setEmail(evento) {
-    this.setState({email: evento.target.value});
-  }
-
-  setSenha(evento) {
-    this.setState({senha: evento.target.value});
-  }
 
   render() {
     return (
@@ -82,42 +28,7 @@ class App extends Component {
             <div className="header">
                 <h1>Cadastro de Autores</h1>
             </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="POST">
-                  <InputCustomizado id="nome" type="text" name="nome" label="Nome" value={this.state.nome} onChange={this.setNome} />
-                  <InputCustomizado id="email" type="email" name="email" label="Email" value={this.state.email} onChange={this.setEmail} />
-                  <InputCustomizado id="senha" type="password" name="senha" label="Senha" value={this.state.senha} onChange={this.setSenha} />
-                  <div className="pure-control-group">
-                    <label></label>
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                  </div>
-                </form>
-
-              </div>
-              <div>
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      this.state.lista.map(function(autor) {
-                        return (
-                          <tr key={autor.id}>
-                            <td>{autor.nome}</td>
-                            <td>{autor.email}</td>
-                          </tr>
-                        );
-                      })
-                    }
-                  </tbody>
-                </table>
-              </div>
-            </div>
+            <AutorBox />
         </div>
     </div>
     );
