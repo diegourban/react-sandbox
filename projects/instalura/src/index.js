33f62a641264e8812f7716ed5a4c720f76b09635
Diego Leonardo Urban <urban@dynamix.com.br>
2017-09-22 12:33:25 -0300

    login update

--- projects/instalura/src/index.js
+++ projects/instalura/src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
 import App from './App';
+import Login from './components/Login';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/timeline" component={App} />
+      </Switch>
+    </BrowserRouter>
+  ),
   document.getElementById('root')
 );
